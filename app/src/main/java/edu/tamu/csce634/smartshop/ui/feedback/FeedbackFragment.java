044a2070a2e0843c229f2d38ceac6e9cdae1fae7
Guiyan He <im.guiyanhe@gmail.com>
2025-10-17 16:48:22 -0500

    update: init profile and feedback pages

---
+++ app/src/main/java/edu/tamu/csce634/smartshop/ui/feedback/FeedbackFragment.java
@@ -0,0 +1,23 @@
+package edu.tamu.csce634.smartshop.ui.feedback;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import edu.tamu.csce634.smartshop.R;
+
+public class FeedbackFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_feedback, container, false);
+    }
+}
+
---
+++ app/src/main/java/edu/tamu/csce634/smartshop/ui/profile/ProfileFragment.java
@@ -0,0 +1,22 @@
+package edu.tamu.csce634.smartshop.ui.profile;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import edu.tamu.csce634.smartshop.R;
+
+public class ProfileFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+}
