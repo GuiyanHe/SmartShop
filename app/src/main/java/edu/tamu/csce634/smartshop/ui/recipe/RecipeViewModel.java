b59cc7dda4f8c931ec73f6643b403e6dd0b83beb
Guiyan He <im.guiyanhe@gmail.com>
2025-10-17 15:49:08 -0500

    update: init

--- app/src/main/java/edu/tamu/csce634/smartshop/ui/recipe/RecipeViewModel.java
+++ app/src/main/java/edu/tamu/csce634/smartshop/ui/recipe/RecipeViewModel.java
@@ -1,16 +1,16 @@
-package edu.tamu.csce634.smartshop.ui.dashboard;
+package edu.tamu.csce634.smartshop.ui.recipe;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class RecipeViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public RecipeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mText.setValue("This is notifications fragment");
     }
 
     public LiveData<String> getText() {
