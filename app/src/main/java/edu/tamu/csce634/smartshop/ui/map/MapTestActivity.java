af787d4e17ef7a6440b677f7427d710649c99642
yaoya2618-dot <yaoya2618@tamu.edu>
2025-11-02 15:15:17 -0600

    add map test activity (deleted after testing)

---
+++ app/src/main/java/edu/tamu/csce634/smartshop/ui/map/MapTestActivity.java
@@ -0,0 +1,28 @@
+package edu.tamu.csce634.smartshop.ui.map;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import edu.tamu.csce634.smartshop.R;
+//Only to test the map page; delete if possible
+
+public class MapTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_test);
+
+        // 动态加载 MapFragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new MapFragment())
+                    .commit();
+        }
+    }
+}
